3f00d4b00836ce19acb12999ff0b2997ad34a902
Andrew Crosby <awcrosby@gmail.com>
2018-04-03 17:49:10 +0000

    no mobile scrolling on tables, styled search bar and navlinks

--- djangosite/home/forms.py
+++ djangosite/home/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import QueryLoc, ScraperParams
 
 class UserQueryForm(forms.Form):
-    query = forms.CharField(label='Query', max_length=100)
+    query = forms.CharField(label='', max_length=30)
 
 class ScraperForm(forms.Form):
     params = forms.ModelChoiceField(queryset=ScraperParams.objects.all().order_by('id'),
